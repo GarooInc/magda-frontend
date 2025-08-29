42570023146004d10f9e8bdf614817ebfdc10409
Jimena Hernández <her21199@uvg.edu.gt>
2025-08-29 12:28:49 -0600

    feat[ui]:FincaDashboard page
    Introduces a reusable Header component with navigation and notification features. Adds the FincaDashboard page and updates AppRouter to include the new route for '/panel-finca'.

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
+import FincaDashboard from '../pages/FincaDashboard';
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="*" element={<Login />} />
+      <Route path="/panel-finca" element={<FincaDashboard />} />
     </Routes>
   );
 };
