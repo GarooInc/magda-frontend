aa0ed308e82d8e58a146202f08f9cf21db83b8ec
Jimena Hernández <her21199@uvg.edu.gt>
2025-11-04 14:24:46 -0600

    Update FormUpload.tsx

--- src/pages/FormUpload.tsx
+++ src/pages/FormUpload.tsx
@@ -1,9 +1,13 @@
 import NotificationForm from "../components/NotificationForm/NotificationForm";
 function FormUpload (){
     return (
-        <div className='h-[100dvh] w-full bg-white p-10 flex justify-center items-center relative'>
-            <img src="/assets/images/logos/logomagda.png" className="w-40 absolute top-4 left-4" alt='logo' />
-            <NotificationForm />
+        <div className='h-[100dvh] w-full bg-white md:p-10 pt-10 px-4 flex flex-col justify-start items-center relative'>
+            <div className="flex justify-start items-start w-full">
+                <img src="/assets/images/logos/logomagda.png" className="w-40" alt='logo' />
+            </div>
+            <div className="flex flex-1 justify-center items-center w-full">
+                <NotificationForm />
+            </div>
         </div>
     )
 }
